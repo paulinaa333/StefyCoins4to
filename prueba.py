15737a5b8072ffdea2484e9445d477d03182b72c
paulinaa333 <piramosbobatto@escuelasproa.edu.ar>
2025-11-12 14:37:14 -0300

    .

--- prueba.py
+++ prueba.py
@@ -2,21 +2,21 @@ from beneficios import tutorias, puntosExtra, cambiarFecha
 from usuario import profesor, estudiante
 from tareas import trabajoPractico
 
-estudiante1 = estudiante(nombre="Stefi",
-                          apellido="Lopez",
+estudiante1 = estudiante(nombre="Paulina",
+                          apellido="Ramos",
                             edad=22, 
                             legajo=1234, 
                             curso="4to A", 
-                            correo="", 
-                            password="", 
+                            correo="piramosbobatto@escuelasproa.edu.ar", 
+                            password="110809", 
                             id=1)
 
-profesor1 = profesor(nombre="Juan", 
-                     apellido="Perez", 
+profesor1 = profesor(nombre="Ignacio", 
+                     apellido="Sanchez", 
                      edad=40, 
-                     especialidad="Matematica", 
-                     correo="", 
-                     password="", 
+                     especialidad="Programacion", 
+                     correo="jisanchezsolano@escuelasproa.edu.ar", 
+                     password="165533", 
                      id=1)
 
 tarea1= trabajoPractico(consigna="esto es una consigna de Tarea1", 
